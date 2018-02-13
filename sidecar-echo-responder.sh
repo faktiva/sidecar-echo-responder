44fb05aa86222df54c465d86154c1e2521063ca4
Emiliano Gabrielli <emiliano.gabrielli@musement.com>
2018-02-13 11:28:05 +0100

    Rework the logic
    Use a pipe to implement an echo responder avoiding the "-e" flag
    which is not portable
    
    Using an echo responder correctly handles connections from LB, avoiding
    SYN flooding

--- sidecar-echo-responder.sh
+++ sidecar-echo-responder.sh
@@ -12,10 +12,16 @@ PONG_PORT="${PONG_PORT:-6666}"
 SERVICE_PROTO="${SERVICE_PROTO:-t}"
 SERVICE_PORT="${SERVICE_PORT:-25}"
 
+# create a pipe to have the client's input back to it
+PIPE_FILE="sidecar-echo-responder.fifo"
+[ -p "${PIPE_FILE}" ] || mknod -m 777 "${PIPE_FILE}" p
+
+# start netcat in listening and keepalive mode
+printf "Starting echo responder on port '%s'\n" "${PONG_PORT}"
+cat "${PIPE_FILE}" | nc -lk -p "${PONG_PORT}" > "${PIPE_FILE}" &
+
 # will exit is no socket is listening on the given port
 while /bin/true ; do
-    ss -ln"$SERVICE_PROTO" | grep ":${SERVICE_PORT}" > /dev/null
-    nc -nvl -p "$PONG_PORT"
-
+    ss -ln"${SERVICE_PROTO}" | grep ":${SERVICE_PORT}" > /dev/null
     sleep 1
 done
